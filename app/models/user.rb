8c4312fd023c89880c39bce420cceda04bdab8a0
revalidate <reva.bhatt@gmail.com>
2016-02-23 21:21:50 -0800

    added many to many relationship into user and place models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :user_places
+  has_many :places, :through => :user_places
+
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
