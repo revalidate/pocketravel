2ca08796d5e76619cc4e29cc8ead2c68b4e19f1a
revalidate <reva.bhatt@gmail.com>
2016-02-25 02:50:23 -0800

    edited nav bar to change colors with scroll

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,4 +2,20 @@ $(document).on("page:change", function(){
  $(".button-collapse").sideNav();
 
  $('.parallax').parallax();
+
+ var scroll_start = 0;
+   var startchange = $('.nav');
+   var offset = startchange.offset();
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.nav').css('background-color', '#0F2743');
+          $('.navbar-default .nav > li > a').css('color', 'white');
+          $('.navbar-default .navbar-header .navbar-brand').css('color', 'white');
+       } else {
+          $('.nav').css('background-color', 'transparent');
+          $('.navbar-default .nav > li > a').css('color', 'black');
+          $('.navbar-default .navbar-header .navbar-brand').css('color', 'black');
+       }
+   });
 });
